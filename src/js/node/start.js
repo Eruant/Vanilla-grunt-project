76a498a4d030cd8a971b553b94243062d2e6c183
Matt Gale <matt@littleball.co.uk>
2013-06-03 15:05:42 +0100

    Added static file server

--- src/js/node/start.js
+++ src/js/node/start.js
@@ -1,23 +1,46 @@
-/*globals require, process */
+/*globals require, process, console */
 
 var http = require('http'),
+	url = require('url'),
+	path = require('path'),
 	fs = require('fs'),
 	port = process.env.PORT || 5000;
 
-fs.readFile('bin/index.html', function (err, html) {
+http.createServer(function (req, res) {
 	'use strict';
+
+	var uri = url.parse(req.url).pathname,
+		filename = path.join(process.cwd(), uri);
 	
-	if (err) {
-		throw err;
-	}
-	http.createServer(function (req, res) {
-	
-		res.writeHead(200, {
-			'Content-Type': 'text/html'
+	path.exists(filename, function (exists) {
+		if (!exists) {
+			res.writeHead(404, {
+				"Content-Type": "text/plain"
+			});
+			res.write("404 Not Found\n");
+			res.end();
+			return;
+		}
+		
+		if (fs.statSync(filename).isDirectory()) {
+			filename += '/index.html';
+		}
+		
+		fs.readFile(filename, 'binary', function (err, file) {
+			if (err) {
+				res.writeHead(500, {
+					"Content-Type": "text/plain"
+				});
+				res.write(err + "\n");
+				res.end();
+				return;
+			}
+			
+			res.writeHead(200);
+			res.write(file, "binary");
+			res.end();
 		});
-		res.write(html);
-		res.end();
+	});
+}).listen(parseInt(port, 10));
 
-	}).listen(port);
-
-});
+console.log("Static file server running at \n => http://localhost:" + port + "\nCTRL + C to shutdown");
